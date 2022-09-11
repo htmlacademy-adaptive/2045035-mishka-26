af6fcf05549ef2e4fc1052f11ea97154f355b92c
Kuza_Ko <cveta-mavk@mail.ru>
2022-09-11 22:23:09 +0800

    добавляет мелкие исправления

--- gulpfile.js
+++ gulpfile.js
@@ -66,10 +66,11 @@ const createWebp = () => {
 
 // SVG
 
-const svg = () =>
-  gulp.src(['source/img/**/*.svg', '!source/img/sprite/*.svg'])
+const svg = () => {
+  return gulp.src(['source/img/**/*.svg', '!source/img/sprite/*.svg'])
     .pipe(svgo())
     .pipe(gulp.dest('build/img'));
+}
 
 const sprite = () => {
   return gulp.src('source/img/sprite/*.svg')
